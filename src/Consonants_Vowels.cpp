25fdb6a1f2c91b37d3ccbd975520104009977ab6
praneeth29 <praneeth29@missionrnd.com>
2016-02-24 22:45:42 +0800

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -17,10 +17,45 @@ Output: consonants should be ??,vowels should be ??
 NOTES: Don't create new string , Dont return anything ,you have been given two pointers ,copy values into those .
 */
 
+#include<stdio.h>
 #include <stddef.h>
+int length(char * str)
+{
+    int i;
+    for(i=0;str[i]!='\0';i++)
+    {
+    }
+    return i;
 
-
+}
+int vowel(char str)
+{
+    if(str=='a' || str=='e' || str=='i' || str=='o' || str=='u' || str=='A' || str=='E' || str=='I' || str=='O' || str=='U')
+        return 1;
+    else return 0;
+}
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
+        (*consonants)=0;
+        (*vowels)=0;
+        if(str==NULL)
+            return;
+        int len=length(str),i;
+        //printf("%d",len);
+        if(len==0)
+            return;
+        for(i=0;i<len;i++)
+        {
+            //printf("\n%c\n",str[i]);
+            if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+            {
+                if(vowel(str[i]))
+                {
+                   // printf("\n%c\n",str[i]);
+                    (*vowels)++;
+                }
+                else (*consonants)++;
+            }
+        }
+       // printf("\n%d   %d",consonants,vowels);
 
 }
